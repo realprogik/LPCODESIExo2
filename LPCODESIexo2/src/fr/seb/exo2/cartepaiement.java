e4a1fed5e0b83c1c3f792acfda7b57e7997bcbfd
c18004286 <c18004286@a-f004248.salsa.univ-amu.fr>
2019-02-15 15:47:57 +0100

    Creation Projet et classe abstraite cartepaiement

---
+++ LPCODESIexo2/src/fr/seb/exo2/cartepaiement.java
@@ -0,0 +1,14 @@
+package fr.seb.exo2;
+
+public abstract class cartepaiement {
+
+	public double payeAchat(double montant) {
+		return montant;
+	}
+	
+	public double autorisePaiement(double montant) {
+		return montant;
+	}
+	
+	
+}
